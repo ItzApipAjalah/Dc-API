c76e99aafcc4b7cbd338c4568d78599b9e20aec5
ItzApipAjalah <70223010+ItzApipAjalah@users.noreply.github.com>
2024-12-13 16:22:27 +0700

    up: getStatusIcon is not defined

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const helpers = require('../utils/helpers');
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+// Add route to serve helper functions
+router.get('/utils/helpers.js', (req, res) => {
+    res.type('application/javascript').send(`
+        window.getStatusIcon = ${helpers.getStatusIcon.toString()};
+        window.getActivityTypeIcon = ${helpers.getActivityTypeIcon.toString()};
+    `);
 });
 
 module.exports = router; 
