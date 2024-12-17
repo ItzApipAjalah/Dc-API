848d0aafeffdef29757640c988a460f9768c113e
ItzApipAjalah <70223010+ItzApipAjalah@users.noreply.github.com>
2024-12-17 15:16:29 +0700

    up : status

--- src/config/discord.js
+++ src/config/discord.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -11,6 +11,48 @@ const client = new Client({
         GatewayIntentBits.GuildWebhooks,
         GatewayIntentBits.MessageContent
     ]
+});
+
+// Set bot status when ready
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    
+    // Set the bot's activity
+    client.user.setPresence({
+        activities: [
+            {
+                name: 'https://discordlookup.amwp.website/',
+                type: ActivityType.Playing,
+                url: 'https://discordlookup.amwp.website/'
+            }
+        ],
+        status: 'online'
+    });
+
+    // Optional: Rotate between multiple statuses
+    const activities = [
+        {
+            name: 'https://discordlookup.amwp.website/',
+            type: ActivityType.Playing
+        },
+        {
+            name: `${client.guilds.cache.size} servers`,
+            type: ActivityType.Watching
+        },
+        {
+            name: 'User Lookups',
+            type: ActivityType.Listening
+        }
+    ];
+
+    let currentActivity = 0;
+    
+    // Change activity every 10 seconds
+    setInterval(() => {
+        const activity = activities[currentActivity];
+        client.user.setActivity(activity.name, { type: activity.type });
+        currentActivity = (currentActivity + 1) % activities.length;
+    }, 10000);
 });
 
 module.exports = client; 
